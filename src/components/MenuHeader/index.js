c1c9f6a331ba6ffbb158289772baebf7d8b7e487
BenXu <xuqiang056@gmail.com>
2022-09-18 01:40:31 +0800

    modify site desc & resize logo

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,8 +1,8 @@
 import logo from "./pandao-logo.png";
 export default function MenuHeader() {
   return (
-    <div style={{ height: "64px", padding: "4px 16px" }}>
-      <img style={{ height: "64px" }} src={logo} alt="logo" />
+    <div style={{ height: "60px", padding: "4px 16px" }}>
+      <img style={{ height: "60px" }} src={logo} alt="logo" />
     </div>
   );
 }
